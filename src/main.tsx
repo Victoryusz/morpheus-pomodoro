6bba41f4c9ef9b5969c43b30c4b1edbc02763735
Victoryusz <hugo4strong@gmail.com>
2025-08-19 20:49:47 -0300

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import { App } from './App'
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hello World!</h1>
+    <App />
   </StrictMode>,
 )
