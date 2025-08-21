ed63429808ef82d2f67fca1b9d1f1f009348f103
Victoryusz <hugo4strong@gmail.com>
2025-08-21 19:23:43 -0300

    style: generic

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,11 +1,11 @@
-import { LucideDraftingCompass } from 'lucide-react';
+import { Atom } from 'lucide-react';
 import styles from './styles.module.css';
 
 export function Logo() {
   return (
     <div className={styles.logo}>
       <a className={styles.logoLink} href='#'>
-        <LucideDraftingCompass />
+        <Atom />
         <span>Morpheus</span>
       </a>
     </div>
